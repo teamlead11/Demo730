e3ad0dc2db77a86fad39c0f04561bc3bb063cbee
teamlead11 <automationlead11@gmail.com>
2019-05-01 08:36:19 +0530

    added new program

--- src/test/Demo.java
+++ src/test/Demo.java
@@ -5,6 +5,11 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("add ur program");
+
+	}
+
+	public static void someMethod() {
+		System.out.println("added a new program");
 	}
 
 }
